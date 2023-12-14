88bd728b5bb201c7d64df7ce4696f87d0d140a73
jae-heo <gjwodud119@gmail.com>
2023-12-14 15:18:15 +0000

    Add: 각 Task별 Train 가능하도록 스크립트 파일 추가

---
+++ train_mask.sh
@@ -0,0 +1,7 @@
+python train.py \
+--data_dir /data/ephemeral/home/data/train/images \
+--model_dir /data/ephemeral/home/project/repo/level1-imageclassification-cv-06/ \
+--model_type mask_model \
+--model BaseModel \
+--dataset MaskModelDataset \
+--criterion focal
