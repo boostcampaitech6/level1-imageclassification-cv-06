e557b04ae07d3c230fdcabf0c82821afed4a2427
jae-heo <gjwodud119@gmail.com>
2023-12-15 05:24:00 +0000

    Add: inference를 위한 shell file 추가

---
+++ inference.sh
@@ -0,0 +1,7 @@
+python inference.py \
+--data_dir /data/ephemeral/home/data/eval \
+--model_dir /data/ephemeral/home/project/repo/level1-imageclassification-cv-06/best_model \
+--output_dir ./ \
+--age_model MyModel \
+--gender_model MyModel \
+--mask_model MyModel \
