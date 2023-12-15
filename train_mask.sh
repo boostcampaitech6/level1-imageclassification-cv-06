0f5c155608c1b45d14dfe630838ed014c5efa270
jae-heo <gjwodud119@gmail.com>
2023-12-15 05:26:35 +0000

    Update: mask train 쉘 스크립트 업데이트 (에폭 설정 추가 및 모델 변경
    BaseModel -> MyModel

--- train_mask.sh
+++ train_mask.sh
@@ -2,6 +2,7 @@ python train.py \
 --data_dir /data/ephemeral/home/data/train/images \
 --model_dir /data/ephemeral/home/project/repo/level1-imageclassification-cv-06/ \
 --model_type mask_model \
---model BaseModel \
+--model MyModel \
 --dataset MaskModelDataset \
---criterion focal
+--criterion focal \
+--epochs 5 \
