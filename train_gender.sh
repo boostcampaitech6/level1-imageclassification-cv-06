a3f6eedd1ab75969b57a3c6ede75f5dcee466b58
kangshwan <rnltls97@gmail.com>
2023-12-17 02:34:43 +0000

    fix: shell file git으로 트랙되는것 제거, .gitignore 파일 최신화 (pre-commit yaml파일 등)
    #2

--- train_gender.sh
+++
@@ -1,7 +0,0 @@
-python train.py \
---data_dir /data/ephemeral/home/data/train/images \
---model_dir /data/ephemeral/home/project/repo/level1-imageclassification-cv-06/ \
---model_type gender_model \
---model BaseModel \
---dataset GenderModelDataset \
---criterion focal
