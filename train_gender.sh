9fc60a60530bbe5b6bc70bc82cc74f0f43cb8089
Junseokko <juniboy970317@gmail.com>
2023-12-19 14:49:05 +0900

    Feat : train_gender.sh변경

--- train_gender.sh
+++ train_gender.sh
@@ -1,7 +1,23 @@
 python train.py \
 --data_dir /data/ephemeral/home/data/train/images \
---model_dir /data/ephemeral/home/project/repo/level1-imageclassification-cv-06/ \
+--model_dir /data/ephemeral/home/project/level1-imageclassification-cv-06 \
 --model_type gender_model \
---model BaseModel \
+--model  EfficientNetb0Custom \
 --dataset GenderModelDataset \
---criterion focal
+--criterion focal \
+--epochs 25 \
+--augmentation BaseAugmentation \
+--resize 224 224 \
+--batch_size 64 \
+--valid_batch_size 100 \
+--optimizer Adam \
+--lr 1e-3 \
+--val_ratio 0.2
+
+# python train.py \
+# --data_dir /data/ephemeral/home/data/train/images \
+# --model_dir /data/ephemeral/home/project/repo/level1-imageclassification-cv-06/ \
+# --model_type gender_model \
+# --model ResNet50Model \
+# --dataset MaskBaseDataset \
+# --criterion focal
